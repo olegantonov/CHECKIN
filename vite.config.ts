7899209ec1a2d50ffe40e618010f21dbc25de0ea
Daniel Marques <daniel.marques.silva@live.com>
2025-10-28 18:20:31 -0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/olegantonov/CHECKIN/tree/main',
+      base: '/CHECKIN/',
       server: {
         port: 3000,
         host: '0.0.0.0',
